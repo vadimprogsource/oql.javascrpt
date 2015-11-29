bf9630e5234ece43e4f56a55c99c42099a3874fb
Dementey Vadim <dementey@outlook.com>
2015-11-29 15:24:36 +0300

    remove index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var sqlProvider       = require('./lib/sqlProvider');
-var expressionBuilder = require('./lib/expressionBuilder');
-var objectBuilder = require('./lib/objectBuilder');
-var queryBuilder  = require('./lib/queryBuilder') ;
-
-module.exports = {sqlProvider : sqlProvider , expressionBuilder: expressionBuilder,objectBuilder: objectBuilder, queryBuilder: queryBuilder};
